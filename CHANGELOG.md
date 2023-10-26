88253c7767eed3e082577d0a823b936fd889aeab
medilies <medilies@gmail.com>
2023-10-26 16:07:37 +0100

    from row-bloom

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+All notable changes to `chrome-php-renderer` will be documented in this file.
---
+++ tests/Pest.php
@@ -0,0 +1,48 @@
+<?php
+
+use RowBloom\ChromePhpRenderer\ChromePhpRenderer;
+use RowBloom\RowBloom\RowBloomServiceProvider;
+use RowBloom\RowBloom\Support;
+
+app()->make(RowBloomServiceProvider::class)->register();
+app()->make(RowBloomServiceProvider::class)->boot();
+
+app()->make(Support::class)->registerRendererDriver(ChromePhpRenderer::NAME, ChromePhpRenderer::class);
+
+uses()
+    ->beforeEach(function () {
+        // app()->forgetInstances();
+        Mockery::close();
+    })
+    ->beforeAll(function () {
+        $folderPath = __DIR__.'/temp';
+        if (! is_dir($folderPath)) {
+            mkdir($folderPath, 0777, true);
+        }
+    })
+    ->afterAll(function () {
+        $folderPath = __DIR__.'/temp';
+        deleteFolder($folderPath);
+    })
+    ->in('feature', 'unit');
+
+function deleteFolder($folderPath)
+{
+    if (! is_dir($folderPath)) {
+        return;
+    }
+
+    $files = array_diff(scandir($folderPath), ['.', '..']);
+
+    foreach ($files as $file) {
+        $filePath = $folderPath.'/'.$file;
+
+        if (is_dir($filePath)) {
+            deleteFolder($filePath);
+        } else {
+            unlink($filePath);
+        }
+    }
+
+    rmdir($folderPath);
+}
